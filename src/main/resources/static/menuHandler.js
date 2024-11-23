a88f80ad12b0b944220120a4b6c12759c17bd0b7
Kucinski345 <kacper.kucinski1b@gmail.com>
2024-11-23 19:31:03 +0100

    server

--- src/main/resources/static/menuHandler.js
+++ src/main/resources/static/menuHandler.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
     menuList.appendChild(mainPageLink);
 
     console.log("Wysłanie zapytania na /auth/check...");
-    fetch(`http://localhost:8080/api/auth/check`, {
+    console.log(`Fetching from: ${API_BASE_URL}/api/auth/check`);
+    fetch(`${API_BASE_URL}/api/auth/check`, {
         method: "GET",
         credentials: "include",
     })
@@ -41,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 .getElementById("logout-button")
                 .addEventListener("click", function () {
                     console.log("Kliknięcie przycisku Wyloguj. Wysyłanie zapytania...");
-                    fetch(`http://localhost:8080/api/auth/logout`, {
+                    fetch(`${API_BASE_URL}/api/auth/logout`, {
                         method: "DELETE",
                         credentials: "include",
                     })
